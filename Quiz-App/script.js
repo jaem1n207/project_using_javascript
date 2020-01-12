b0ac77a9a8ad4177bf5a2cc1404a3cef9d958842
jaem1n207 <jaem1n207@naver.com>
2020-01-12 23:57:08 +0900

    [UPDATE] script.js

--- Quiz-App/script.js
+++ Quiz-App/script.js
@@ -1,30 +1,87 @@
 const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
-const shuffledQuestions, currentQustionIndex
+let shuffledQuestions, currentQustionIndex;
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQustionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
   startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQustionIndex = 0;
   questionContainerElement.classList.remove("hide");
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQustionIndex = 0
   setNextQuestion();
 }
 
 function setNextQuestion() {
-
+  resetState();
+  showQuestion(shuffledQuestions[currentQustionIndex]);
 }
 
-function selectAnswer() {}
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBUtton = e.target;
+  const correct = selectedBUtton.dataset.correct;
+  setStatusClass(document.body, correct);
+
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQustionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
 
 const questions = [
   {
-    question: "What is 2 + 2",
+    question: "What is 2 + 2?",
     answers: [
-      {text: "4", correct: true},
-      {text: "22", correct: false}
+      { text: "4", correct: true },
+      { text: "22", correct: false }
     ]
   }
-]
+];
