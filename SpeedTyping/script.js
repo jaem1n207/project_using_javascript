9ae088cd234bd23486558624a20186e644edc04f
jaem1n207 <jaem1n207@naver.com>
2020-01-17 13:33:07 +0900

    [ADD] setTimer

--- SpeedTyping/script.js
+++ SpeedTyping/script.js
@@ -1,6 +1,7 @@
 const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
 const quoteDisplayElement = document.getElementById("quoteDisplay");
 const quoteInputElement = document.getElementById("quoteInput");
+const timerElement = document.getElementById("timer");
 
 /* 
   한 글자씩 비교하여 class를 추가해준다.
@@ -8,16 +9,25 @@ const quoteInputElement = document.getElementById("quoteInput");
 quoteInputElement.addEventListener("input", () => {
   const arrayQuote = quoteDisplayElement.querySelectorAll("span");
   const arrayValue = quoteInputElement.value.split("");
+  let correct = true;
+
   arrayQuote.forEach((characterSpan, index) => {
     const character = arrayValue[index];
-    if (character === characterSpan.innerText) {
+    if (character == null) {
+      characterSpan.classList.remove("corect");
+      characterSpan.classList.remove("incorrect");
+      correct = false;
+    } else if (character === characterSpan.innerText) {
       characterSpan.classList.add("correct");
       characterSpan.classList.remove("incorrect");
     } else {
       characterSpan.classList.remove("correct");
       characterSpan.classList.add("incorrect");
+      correct = false;
     }
   });
+
+  if (correct) renderNewQuote();
 });
 
 /* 
@@ -41,6 +51,23 @@ async function renderNewQuote() {
     quoteDisplayElement.appendChild(characterSpan);
   });
   quoteInputElement.value = null;
+  startTimer();
+}
+
+/* 
+  Timer를 시작한다.
+*/
+let startTime;
+function startTimer() {
+  timerElement.innerText = 0;
+  startTime = new Date();
+  setInterval(() => {
+    timer.innerText = getTimerTime();
+  }, 1000);
+}
+
+function getTimerTime() {
+  return Math.floor((new Date() - startTime) / 1000);
 }
 
 renderNewQuote();
